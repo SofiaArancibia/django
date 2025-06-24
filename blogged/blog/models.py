1ba04ee90f4186945dd0aadf598e0038f0dff2e9
Sofia C. Arancibia <sofi.aran_05@hotmail.com>
2025-06-24 20:26:57 +0200

    add and show comments

--- blogged/blog/models.py
+++ blogged/blog/models.py
@@ -15,3 +15,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # Link comment to a post
+    # on_delete=models.CASCADE means that if the post is deleted, all its comments will also be deleted
+    name = models.CharField(max_length=255)  # Author of the comment
+    content = models.TextField()  # Content of the comment
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the date when the comment is created
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f'{self.name} - {self.post.title}'
--- blogged/blog/views.py
+++ blogged/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,14 @@ def index(request):
 def detail(request, slug):
     # post = Post.objects.get(slug=slug)  # Fetch the post with the given slug. This will crush the website if the slug does not exist
     post = get_object_or_404(Post, slug=slug)  # Fetch the post with the given slug or return a 404 error if not found
-    return render(request, 'blog/detail.html', {'post': post})  # Render the detail template with the post context
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False) # Create a comment instance but don't save it to the database yet
+            comment.post = post
+            comment.save()  # Save the comment to the database
+            return redirect('detail', slug=slug)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/detail.html', {'post': post,
+                                                'form': form})  # Render the detail template with the post context
